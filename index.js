568bb8ba4e51d840a52d888e66ccad0adc5d9c78
Mildrette <mildredfonka1@gmail.com>
2023-05-06 00:50:56 +0100

    added hovering button

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
+var storeButton = document.getElementById('playstore');
+storeButton.style.backgroundColor = 'white';
+
+// trigger this function every time the user scrolls
+window.onscroll = function (event) {
+    var scroll = window.pageYOffset;
+    if (scroll < 100) {
+        storeButton.style.backgroundColor = 'white';
+    } else if (scroll >= 100 && scroll < 400) {
+        storeButton.style.backgroundColor = 'orange';
+    } else if (scroll >= 400 && scroll < 1200) {
+        storeButton.style.backgroundColor = 'orange';
+} 
     }
-  }

78be0c7479a3152de38f53e11a8ef79180f57211
Mildrette <mildredfonka1@gmail.com>
2023-08-30 10:12:03 +0100

    deku

---
+++ src/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FaGooglePlay} from 'react-icons/fa';
+
+
+const Headers = () => {
+  return (
+    <body>
+      <section className="section-hero">
+        <div className="hero">
+          <div className="hero-text-box">
+            <h1 className='heading-primary'>Your Ultimate Secure Messaging Solution</h1>
+              <hr />
+           
+            <p className='hero-description'>Discover Deku SMS, the groundbreaking open-source 
+                SMS app that redefines communication. With military-grade encryption, 
+                innovative features, and a community-driven spirit, 
+                Deku SMS is your gateway to a new era of secure messaging.</p>
+
+
+            <a href="https://play.google.com/store/apps/details?id=com.afkanerd.deku" className="btn btn--fill margin-right-btn">
+             DOWNLOAD <FaGooglePlay />
+            </a>
+            <a href="#us" className="btn btn--outline margin-right-btn">
+              Learn more &darr;
+            </a>
+          </div>
+          {/* <div className="hero-img-box">
+            <img
+              src="https://prayagtandon.github.io/Omnifood-Project/Hero-section/img/hero.png"
+              alt="Woman enjoying food, meals in storage container, and food bowls on a table"
+              className="hero-img"
+            />
+          </div> */}
+      
+        </div>
+      </section>
+    </body>
+  );
+};
+
+export default Headers;
